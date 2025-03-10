ca3de0a340a6bdbf0a9f10c2f99e058ab0beba4a
arjunTltech <arjunntltechnologies@gmail.com>
2025-03-10 14:34:59 +0530

    error 503 triggered

--- server.js
+++ server.js
@@ -2,27 +2,45 @@ import express from "express";
 import 'dotenv/config';
 import cors from "cors";
 import { app,  server } from './socket/socket.js'
-
 import adminRoutes from './routes/admin/index.route.js';
 import webRoutes from './routes/web/index.route.js';
+import errorHandler from "./middlewares/errorHandler.js";
+import prisma from "./helpers/prisma.js";
 
 
 const PORT = process.env.PORT;
 
-
+// Middleware
 app.use(cors('*')); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-
-// admin routes
-app.use("/api/v1/admin", adminRoutes);
-app.use('/api/v1/web', webRoutes)
-
+// Health Check Route
 app.get("/", (req, res) => {
   res.send("VSG RUNNING");
 });
+
+
+// Database Connection Check Middleware
+app.use(async (req, res, next) => {
+  try {
+    await prisma.$queryRaw`SELECT 1`;
+    next();
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+    return res.status(503).json({
+      success: false,
+      message: "Service Unavailable. The database connection failed.",
+    });
+  }
+});
+
+// API Routes
+app.use("/api/v1/admin", adminRoutes);
+app.use('/api/v1/web', webRoutes)
+
+// Global Error Handling Middleware
+app.use(errorHandler);
 
 server.listen(PORT , () => {
   console.log(`Server is running on port  http://localhost:${PORT}`);
